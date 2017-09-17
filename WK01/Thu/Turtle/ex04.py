6c241fff25f700c1dd3f27ab7712aa8753e9b7d9
Monis Rizvi <monisriz@gmail.com>
2017-09-17 16:40:50 -0500

    added comments

--- WK01/Thu/Turtle/ex04.py
+++ WK01/Thu/Turtle/ex04.py
@@ -1,25 +1,27 @@
 # Exercise 4    A night sky full of stars
 import random
 from turtle import *
-# randx = random.randrange(0,401)
-# randy = random.randrange(0,301)
-# sz = random.randrange(5, 30)
-starqty = random.randrange(50, 200)
-bgcolor("black")
-color("white", "white")
-speed(100)
+setup (width=800, height=800)
+
+starqty = random.randrange(50, 100)         # random number of stars drawn
+bgcolor("black")                            # background color
+color("white", "white")                     # pen and fill color
+speed(0)                                    # set speed for faster animation
+hideturtle()                                # hide turtle for a cleaner look
 
 for i in range(starqty):
-    randx = random.randrange(-400,401)
-    randy = random.randrange(-300,301)
+    randx = random.randrange(-390,390)      # random x-coordinate
+    randy = random.randrange(-390,391)      # random y-coordinate
     penup()
-    goto(randx, randy)
+    goto(randx, randy)                      # go to a random drawing position
     pendown()
-    sz = random.randrange(5, 30)
+    sz = random.randrange(5, 15)            # random star size
     begin_fill()
-    for i in range(5):
+    for i in range(5):                      # function to draw a star
         forward(sz)
-        left(144)
+        left(72)
+        forward(sz)
+        right(144)
     end_fill()
     penup()
 
