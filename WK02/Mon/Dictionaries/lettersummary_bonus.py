1058c1e14a300cf977c9e29d7bed6297886dcff4
Monis Rizvi <monisriz@gmail.com>
2017-10-30 17:10:48 -0500

    Week 02 Exercises

--- WK02/Mon/Dictionaries/lettersummary_bonus.py
+++ WK02/Mon/Dictionaries/lettersummary_bonus.py
@@ -1,14 +1,16 @@
 # Bonus challenge - print top 3 values of the letter count
 
 import string
-count = {}
 
+count = {}
 def letter_histogram():
     word = input("Enter a word: ").lower()
     for achar in word:
         if achar in string.ascii_lowercase:
-            count_letter = count.get(achar, 0)
-            count[achar] = count_letter + 1
+            if achar not in count:
+                count[achar] = 1
+            else:
+                count[achar] += 1
     # print(count)
     count_list = list(count.items())
     print(count_list)
